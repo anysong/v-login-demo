59fee97fffb3066c0257587ef4ac17850007e2e0
dengyingyi2 <dengyingyi@jd.com>
2019-01-09 17:39:57 +0800

    dev

--- src/app/login/src/index.js
+++ src/app/login/src/index.js
@@ -3,10 +3,9 @@
 // import '../../../components/header/style.css';
 // import myImg from '../../../components/header/i/logo.jpeg';
 
-import header from '../../../components/header'; //引入模块
+// import header from '../../../components/header'; //引入模块
 // import footer from '../../../components/footer'; //引入模块
 
-console.log('header', header);
 new Vue({
     "el": "#app",
     "data": {
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,4 @@
+import './style.css';
 
 export default {
     aaa: 2222
